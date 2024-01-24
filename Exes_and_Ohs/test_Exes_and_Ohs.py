4433bc862a6930a15d70451ad34191d76558073a
AlexNash96 <alexnash96@gmail.com>
2024-01-23 18:50:14 -0600

    changed Exes_and_Ohs

--- Exes_and_Ohs/test_Exes_and_Ohs.py
+++ Exes_and_Ohs/test_Exes_and_Ohs.py
@@ -4,7 +4,7 @@ def xo(s):
     return s.count('x')+s.count('X') == s.count('o')+s.count('O')
 
 def test_answer():
-    strings = 'xxoo','oxoxfhsdaflkjqxjaasdfofhexo','asdfjklh','dhxkdjeiox'
+    strings = 'xxoo','oxoXfhsdaflkjqxjaasdfOfhexO','asdfjklh','dhxkdjeiOx'
     answers = True,True,True,False
     for i in range(len(strings)):
         assert xo(strings[i]) == answers[i]
